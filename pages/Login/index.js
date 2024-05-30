def42a87005c6b23b2af96d372cc1667af632c4e
geraldohomero <geraldohomero@protonmail.com>
2024-05-30 19:20:16 -0300

    update Login

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Image, Text, Alert } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios'; // Add this line
+import axios from 'axios';
 import API_URLS from '../../config/apiUrls';
 
 export default function Login({ navigation }) {
@@ -19,7 +19,7 @@ export default function Login({ navigation }) {
     const data = response.data;
     const token = data.jwtToken;
 
-    if (token) {
+    if (token && typeof token === 'string') {
       console.log('Token JWT via E-mail: ' + token);
       // Redefine o histórico de navegação após o login
       navigation.reset({
@@ -27,14 +27,14 @@ export default function Login({ navigation }) {
         routes: [{ name: 'MyTabs' }],
       });
     } else {
-      throw new Error('Token não encontrado');
+      throw new Error('Token não encontrado ou não é uma string');
     }
 
     // Armazena o token JWT no AsyncStorage
     try {
       await AsyncStorage.setItem('token', token);
     } catch (error) {
-      console.error('Erro ao armazenar o token:', error);
+      // handle error
     }
   };
 
